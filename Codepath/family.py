a49fbda8a6f3edd26a48d245ada9f821014c51ad
Nayeel Imtiaz <naimtiaz@ucsc.edu>
2022-12-18 01:40:50 -0800

    Implemented real code for family problem

--- Codepath/family.py
+++ Codepath/family.py
@@ -1,3 +1,7 @@
+# family.py
+# Nayeel Imtiaz
+# 12/18/22
+
 '''
 Closest In Age 
 
@@ -12,6 +16,9 @@ family2 = {5, 2, 10, 22}
 Return: 
 {20, 22} 
 '''
+
+import random # To generate pseudorandom lists for input.
+
 def family(family1, family2):
     '''
     Sort both family1 and family2 lists.
@@ -25,7 +32,7 @@ def family(family1, family2):
         x = abs(f1 - f2)
         if x < closest_difference:
             closest_difference = x
-            closest_pair = [left pointer, right pointer]
+            closest_pair = [f1, f2]
         
         if f1 <= f2:
             left pointer ++
@@ -38,7 +45,7 @@ def family(family1, family2):
         x = abs(f1 - f2)
         if x < closest_difference:
             closest_difference = x
-            closest_pair = [left pointer, right pointer]
+            closest_pair = [f1, f2]
         
         left pointer ++
     
@@ -48,19 +55,70 @@ def family(family1, family2):
         x = abs(f1 - f2)
         if x < closest_difference:
             closest_difference = x
-            closest_pair = [left pointer, right pointer]
+            closest_pair = [f1, f2]
         
         right pointer ++
 
     return closest_pair
-        
-
-
-
     '''
 
+    family1.sort()
+    family2.sort()
+    l, r = 0, 0
+    
+    closest_difference = float('infinity')
+    closest_pair = None
+
+    while l < len(family1) and r < len(family2):
+        f1 = family1[l]
+        f2 = family2[r]
+        x = abs(f1-f2)
+        if x < closest_difference:
+            closest_difference = x
+            closest_pair = [f1, f2]
+        
+        if f1 <= f2:
+            l += 1
+        else:
+            r += 1
+    
+    while l < len(family1):
+        f1 = family1[l]
+        f2 = family2[-1]
+        x = abs(f1-f2)
+        if x < closest_difference:
+            closest_difference = x
+            closest_pair = [f1, f2]
+        
+        l += 1
+
+    while r < len(family2):
+        f1 = family1[-1]
+        f2 = family2[r]
+        x = abs(f1-f2)
+        if x < closest_difference:
+            closest_difference = x
+            closest_pair = [f1, f2]
+        
+        r += 1
+    
+    return closest_pair
+
 def main():
-    pass
+    family1 = [50, 15, 20, 55]
+    family2 = [5, 2, 10, 22]
+
+    print(family(family1, family2)) # [20, 22]
+
+    random.seed(69)
+    f3 = [random.randint(1, 100000) for i in range(50)]
+    f4 = [random.randint(1, 100000) for i in range(50)]
+    # print(f3)
+    # print(f4)
+
+    print(family(f3, f4)) # [8435, 8480]
+
+
 
 if __name__ == '__main__':
     main()
